1a5f20be711976b21af069bf29442e1a0ea0aec9
t-a1024 <133000386+t-a1024@users.noreply.github.com>
2024-12-07 12:10:43 +0900

    警告を解消

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,13 @@
 import type { Metadata } from "next";
-import Head from "next/head";
 
 import "./globals.css";
-
 
 export const metadata: Metadata = {
   title: "コメント採点",
   description: "コメントを採点するよ",
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({
@@ -16,12 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja">
-      <Head>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-    <body className="container mx-auto p-4">                      
+      <body className="container mx-auto p-4">                      
         {children}             
-    </body>
-  </html>
+      </body>
+    </html>
   );
 }
